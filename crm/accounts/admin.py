0b8f027d57ea865def9b60eb8042219485376e0f
VedantBandre <vedantbandre33@gmail.com>
2025-08-22 22:53:44 +0200

    created models and relationships

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Registering the models created in models.py
+from .models import *
+admin.site.register(Customer)
+admin.site.register(Tag)
+admin.site.register(Product)
+admin.site.register(Order)
