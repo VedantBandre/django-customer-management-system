4a3da2aae3166aee482bc7532cb91713e7bf26df
VedantBandre <vedantbandre33@gmail.com>
2025-08-26 14:42:51 +0200

    Password reset email functionality

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -17,4 +20,25 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+    # Password reset Email functionality views
+    # Sumit the password reset email form
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), 
+         name="reset_password"),
+
+    # Password reset Email sent success message
+    path('reset_password_sent/', 
+         auth_views.PasswordChangeDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+         name="password_reset_done",),
+
+    # Link to Password reset for in email
+    path('reset/<uidb64>/<token>', 
+         auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+         name="password_reset_confirm"),
+
+    # Password successfully changed message
+    path('reset_password_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+         name="password_reset_complete"),
 ]
